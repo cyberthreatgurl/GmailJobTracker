edc9dbd8474ea0ad1c809f152504ab34a2a54c91
Adrian Shaw <cyberintelgurl@gmail.com>
2026-02-26 13:27:17 -0500

    chore: bump minor version to 2.5.0

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """Version information for GmailJobTracker."""
 
-__version__ = "2.4.0"
-__version_info__ = (2, 4, 0)
+__version__ = "2.5.0"
+__version_info__ = (2, 5, 0)
 __author__ = "cyberthreatgurl"
 __description__ = (
     "Privacy-first job application tracking powered by Gmail API + ML classification"
